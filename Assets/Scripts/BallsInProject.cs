604610bf1ac47f8451a157fdf1b74b6ecf306198
muthuGammer <muthuvijayakumar5@gmail.com>
2020-05-16 23:45:27 +0530

    Do tween added - Ball Selection Screen added - Three different method implement for ball selection

---
+++ Assets/Scripts/BallsInProject.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public enum MethodType { Method1, Method2, Method3 }
+public class BallsInProject : MonoBehaviour
+{
+    public static BallsInProject instance;
+
+    public List<Balls> _listOfBalls = new List<Balls>();
+
+    public GameObject scrollBar;
+    public GameObject ballPanel;
+    public SampleBallCard sampleCard;
+    public GameObject previewObject;
+    public GameObject curPreviewItem;
+    public GameObject playButton;
+    public Transform endPoint,startPoint;
+    private List<SampleBallCard> _listOfSpawnedCards = new List<SampleBallCard>();
+
+    public MethodType curMethod;
+
+    public void Start()
+    {
+        instance = GetComponent<BallsInProject>();
+
+        if (curMethod == MethodType.Method3)
+        {
+            playButton.SetActive(true);
+        }
+        else
+        {
+            playButton.SetActive(false);
+        }
+
+        for (int i = 0; i < _listOfBalls.Count; i++)
+        {
+            SampleBallCard temp = Instantiate(sampleCard, scrollBar.transform);
+            temp.transform.SetParent(scrollBar.transform);
+            temp.thumbnailImg.sprite = _listOfBalls[i].thumbnail;
+            temp.myBall = _listOfBalls[i].ballPrefab;
+            temp.myProperties = _listOfBalls[i];
+            _listOfSpawnedCards.Add(temp);
+        }
+    }
+
+    public void SetPreview(int id)
+    {
+        if (curPreviewItem)
+        {
+            Destroy(curPreviewItem);
+        }
+
+        curPreviewItem = Instantiate(_listOfSpawnedCards[id].myBall);
+        curPreviewItem.transform.SetParent(previewObject.transform);
+       curPreviewItem.transform.localPosition = Vector3.zero;
+       curPreviewItem.transform.localScale = Vector3.one;
+
+    }
+
+    public void MoveDown()
+    {
+        // ballPanel.transform.DOLocalMoveY(30f,.01f);
+        ballPanel.transform.DOLocalMove(endPoint.localPosition, 1);//.OnComplete(() => AfterCompletion());
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(playButton.transform.DOScale(0.75f, 0.5f))
+          .Append(playButton.transform.DOScale(1f, 0.5f)).OnComplete(()=>playButton.SetActive(false));
+
+
+        Invoke("MoveUp",3f);
+    }
+
+    public void MoveUp()
+    {
+        playButton.SetActive(true);
+        // ballPanel.transform.DOLocalMoveY(30f,.01f);
+        ballPanel.transform.DOLocalMove(startPoint.localPosition, 1);//.OnComplete(() => AfterCompletion());
+    }
+
+   
+}
---
+++ Assets/Scripts/SwipeMechanism.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeMechanism : MonoBehaviour
+{
+
+    public GameObject scrollBar;
+    float scrol_Pos = 0;
+    float[] pos;
+
+    public int curId=-1, prevId=-1;
+
+    public void Update()
+    {
+        if(BallsInProject.instance.curMethod==MethodType.Method2)
+        {
+            pos = new float[transform.childCount];
+            float dis = 1f / (pos.Length - 1);
+
+            for (int i = 0; i < pos.Length; i++)
+            {
+                pos[i] = dis * i;
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                scrol_Pos = scrollBar.GetComponent<Scrollbar>().value;
+            }
+            else
+            {
+                for (int i = 0; i < pos.Length; i++)
+                {
+                    if (scrol_Pos < pos[i] + (dis / 2) && scrol_Pos > pos[i] - (dis / 2))
+                    {
+                        scrollBar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollBar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
+                    }
+                }
+            }
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (scrol_Pos < pos[i] + (dis / 2) && scrol_Pos > pos[i] - (dis / 2))
+                {
+                   // curId = i;
+                    if(curId!=i)
+                    {
+                        prevId=curId;
+                        curId = i;
+                        print(i);
+                        BallsInProject.instance.SetPreview(i);
+                    }
+                    transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
+                    for (int a = 0; a < pos.Length; a++)
+                    {
+                        if (a != i)
+                        {
+                            transform.GetChild(a).localScale = Vector2.Lerp(transform.GetChild(a).localScale, new Vector2(0.8f, 0.8f), 0.1f);
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+      
+    }
+}
