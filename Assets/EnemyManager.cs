10e5ac33128e8d276f3489d8e3f7d3f3e4891471
muthuGammer <muthuvijayakumar5@gmail.com>
2020-05-17 23:20:50 +0530

    Enemy Managing system implemented need to test

---
+++ Assets/EnemyManager.cs
@@ -0,0 +1,83 @@
+﻿using garagekitgames;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager eManager;
+
+    public EnemyRuntimeSet enemyRuntimeSet;
+    public UnityEvent allEnemiesDead;
+    private List<GameObject> _listOfEnemy = new List<GameObject>();
+    public List<GameObject> _listOfDiedEnemy = new List<GameObject>();
+    private int i;
+
+    public GameObject enemyPrefab;
+    public Transform pos;
+
+
+
+    public void Start()
+    {
+        eManager = GetComponent<EnemyManager>();
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SpawnEnemies(5f, pos);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _listOfEnemy[0].GetComponent<Enemy_Dunk>().OnDamage(25f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            enemyRuntimeSet.Items.Clear();
+        }
+    }
+
+    #region Enemy Spawn Function
+
+    public void SpawnEnemies(float _listOfEnemies, Transform pos)
+    {
+        for (i = 0; i < _listOfEnemies; i++)
+        {
+            GameObject temp = Instantiate(enemyPrefab, pos);
+            float x = Random.Range(-5, 5);
+            float y = Random.Range(-5, 5);
+            temp.transform.localPosition = new Vector3(x, y, Random.Range(0, 5));
+            _listOfEnemy.Add(temp);
+            temp.GetComponent<Enemy_Dunk>().OnSpawned();
+        }
+    }
+
+    #endregion
+
+    #region New Segment
+
+    public void OnNewSegmentStarted()
+    {
+        if (!(enemyRuntimeSet.Items.Count > 0))
+        {
+            allEnemiesDead.Invoke();
+            enemyRuntimeSet.Items.Clear();
+        }
+    }
+
+    public void CheckAllEnemiesDiedBeforeReaching()
+    {
+        if (!(enemyRuntimeSet.Items.Count > 0))
+        {
+            allEnemiesDead.Invoke();
+            enemyRuntimeSet.Items.Clear();
+        }
+    }
+   
+    #endregion
+
+}
---
+++ Assets/Scripts/Enemy_Dunk.cs
@@ -0,0 +1,45 @@
+﻿using garagekitgames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Dunk : MonoBehaviour
+{
+    public float enemyHP;
+    public bool isEnemyAlive;
+    public EnemyRuntimeSet enemyRuntimeSet;
+
+
+    #region Enemy Spawn 
+    public void OnSpawned()
+    {
+        isEnemyAlive = true;
+        enemyRuntimeSet.Add(this);
+    }
+    #endregion
+
+    #region Enemy Damage and life functions 
+    public void OnDamage(float damage)
+    {
+        enemyHP -= damage;
+        CheckAlive();
+    }
+
+    public void CheckAlive()
+    {
+        if (enemyHP <= 0)
+        {
+            OnDied();
+        }
+    }
+
+    public void OnDied()
+    {
+        isEnemyAlive = false;
+        enemyRuntimeSet.Remove(this);
+        EnemyManager.eManager._listOfDiedEnemy.Add(this.gameObject);
+        EnemyManager.eManager.CheckAllEnemiesDiedBeforeReaching();
+    }
+    #endregion
+
+}
