b0ef9d1d7d2e56f85f5ffcb778291038619668eb
garagekitgames <garagekitgames@gmail.com>
2020-05-19 14:49:00 +0530

    small changes

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using SO;
+using garagekitgames;
 public class Util : MonoBehaviour
 {
-
+    public EnemyRuntimeSet enemyRuntimeSet;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,7 @@ public class Util : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        enemyRuntimeSet.Items.Clear();
     }
     // Update is called once per frame
     void Update()
