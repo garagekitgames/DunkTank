9eda760c89e9e91dcb9101c6c9edf20c0fb74427
garagekitgames <garagekitgames@gmail.com>
2020-05-21 02:25:50 +0530

    trap base

---
+++ Assets/Scripts/Traps/Trigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+
+    public GameObject myTrapGO;
+    private ITrap myTrap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTrap = this.GetComponent<ITrap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(myTrap != null)
+            {
+                myTrap.OnTriggered();
+            }
+        }
+    }
+}
