87ac933687bc5e959105dad8cd7816f04230d7b3
muthuGammer <muthuvijayakumar5@gmail.com>
2020-06-03 19:58:36 +0530

    New segment imported

---
+++ Assets/BlockHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockHealth : MonoBehaviour
+{
+    public MeshFilter myMesh;
+    public int health;
+    public Mesh[] damageMesh;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            health++;
+
+            CheckHealth();
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    public void CheckHealth()
+    {
+        if (health == 1)
+        {
+            myMesh.mesh = damageMesh[0];
+
+
+        }
+        else if (health == 2)
+        {
+            myMesh.mesh = damageMesh[1];
+
+        }
+        else if (health == 3)
+        {
+            myMesh.mesh = damageMesh[2];
+
+        }
+    }
+}
